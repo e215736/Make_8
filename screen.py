3f4b1dcbfa98fd0d59294c9a7ccf69e27e780c84
Taiga Nishino <e215736@ie.u-ryukyu.ac.jp>
2022-07-29 14:28:03 +0900

    first commit

---
+++ screen.py
@@ -0,0 +1,41 @@
+from kivy.app import App
+
+#「ScreenManager()」は実装直後には一つの画面しか所持していません
+#そのため、別の画面への遷移したいときには「ScreenManager()」に対して「Screen()」を追加します
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+from kivy.uix.widget import Widget
+
+from kivy.properties import StringProperty
+
+class TitleScreen(Screen):
+    pass
+
+
+class MainScreen(Screen):
+    pass
+
+class WrongAnsScreen(Screen):
+    pass
+
+class RetireScreen(Screen):
+    pass
+
+class CorrectScreen(Screen):
+    pass
+
+
+
+class ScreenApp(App):
+    def build(self):
+        self.sm = ScreenManager()
+        self.sm.add_widget(TitleScreen(name='title'))
+        self.sm.add_widget(MainScreen(name='main'))
+        self.sm.add_widget(WrongAnsScreen(name='wrong'))
+        self.sm.add_widget(RetireScreen(name='retire'))
+        self.sm.add_widget(CorrectScreen(name='correct'))
+        return self.sm
+
+
+if __name__ == '__main__':
+    ScreenApp().run()
