01595a2c0fe506d502d927dbe9bf5324dfa482ca
Daisuke Nakama <e215716@ie.u-ryukyu.ac.jp>
2022-08-03 21:50:20 +0900

    add setButtonText

--- model.py
+++ model.py
@@ -5,10 +5,10 @@ class Model:
         self.value = '4839'
         self.view = view #viewへの参照
         
-    def setFormula(self):
+    def setFormula(self): #式を入力
         self.view.showFormula(self.value) #viewへ通知
         
-    def setButton(self):
+    def setButton(self): #式を入力
         self.view.showButton(self.value) #viewへ通知
         
     
