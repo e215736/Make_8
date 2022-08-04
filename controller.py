bd0215d63ab8301533c088f81d783aa6e251c27f
hiroya-ie <e195738@ie.u-ryukyu.ac.jp>
2022-08-04 09:55:39 +0900

    Create "make8.py" based on the old file

--- controller.py
+++ controller.py
@@ -1,9 +1,8 @@
 class Controller:
-    def __init__(self,model): #コンストラクタ
-        self.model = model #modelへの参照
+    def __init__(self,model):
+        self.model = model
         self.model.setFormula() #modelへ通知
         self.model.setButton() #modelへ通知
         
     def onPress(self): #ボタンが押された時の処理
         pass
-       
